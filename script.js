4050fb19784f7a3dc59414cad028fd07a0635be2
Julian2206 <julian.pcc991@gmail.com>
2022-06-13 16:35:54 +0200

    test

---
+++ script.js
@@ -0,0 +1,18 @@
+/**Chiediamo all’utente il suo nome,
+poi chiediamo il suo cognome,
+poi chiediamo il suo colore preferito.
+Infine scrivi sulla pagina nomecognomecolorepreferito22
+(sostituendo nome, cognome e colore preferito con quelli inseriti dall'utente) */
+
+const usrName = window.prompt(" what is your name ? ");
+console.log(usrName);
+
+const lastName = window.prompt(" your last name ? ");
+console.log(lastName);
+
+const favoriteColor = window.prompt(" favorite color ? ");
+console.log(favoriteColor);
+
+//document.getElementById(usrName + lastName + favoriteColor).innerHTML = "test";
+
+//alert(usrName + lastName + favoriteColor);
