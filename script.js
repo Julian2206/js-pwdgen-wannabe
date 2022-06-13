cba527ccc9ca35c6e8358e165d2b23b8f567c258
Julian2206 <julian.pcc991@gmail.com>
2022-06-13 17:17:06 +0200

    mod-general(n-1)

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ poi chiediamo il suo colore preferito.
 Infine scrivi sulla pagina nomecognomecolorepreferito22
 (sostituendo nome, cognome e colore preferito con quelli inseriti dall'utente) */
 
-const usrName = window.prompt(" what is your name ? ");
+const usrName = prompt(" what is your name ? ");
 console.log(usrName);
 
-const lastName = window.prompt(" your last name ? ");
+const lastName = prompt(" your last name ? ");
 console.log(lastName);
 
-const favoriteColor = window.prompt(" favorite color ? ");
+const favoriteColor = prompt(" favorite color ? ");
 console.log(favoriteColor);
 
-//document.getElementById(usrName + lastName + favoriteColor).innerHTML = "test";
+//my passwd
+const myPassword = usrName + lastName + favoriteColor + "22";
+
+document.getElementById("name").innerHTML = usrName;
+document.getElementById("surname").innerHTML = lastName;
+document.getElementById("color").innerHTML = favoriteColor;
+document.getElementById("password").innerHTML = myPassword;
 
 //alert(usrName + lastName + favoriteColor);
